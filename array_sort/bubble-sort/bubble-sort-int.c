00b14e4cb6b8c1fccf805a730c1dabb97eb8c58e
Francisco Cunha Neto <fcunhaneto@gmail.com>
2019-11-24 10:26:43 -0300

    Bubble-sort-int

---
+++ array_sort/bubble-sort/bubble-sort-int.c
@@ -0,0 +1,18 @@
+
+/*
+ * Ordenar array de inteiros pelo método bubble sort.
+ */
+void sort_integer(int *arr, int int_max) {
+    int i = int_max - 1;
+    int j, aux;
+    do {
+        for(j=0; j<i; j++) {
+            if(arr[j] > arr[i]) {
+                aux = arr[i];
+                arr[i] = arr[j];
+                arr[j] = aux;
+            }
+        }
+        i--;
+    } while(i != 0);
+}
